bcd84f07672d34f65b3f945b653b3270f9c1cd9a
Adam Schachne <adamschachne@live.com>
2018-06-10 02:21:31 -0700

    basic whitney music box visualization

--- docs/build.js
+++ docs/build.js
@@ -1,15 +1,51 @@
-var sketch = function (p) {
-    p.preload = function () {
+var sketch = function (p5) {
+    var NUM_POINTS = 50;
+    var radius;
+    var center;
+    var maxCircleWidth;
+    var canvas;
+    var rate;
+    p5.preload = function () {
     };
-    p.setup = function () {
-        p.createCanvas(p.windowWidth, p.windowHeight);
+    function resize() {
+        console.log(p5.windowWidth, p5.windowHeight);
+        radius = 0.8 * (p5.windowWidth / 2);
+        center = {
+            x: p5.windowWidth / 2,
+            y: p5.windowHeight / 2
+        };
+        p5.resizeCanvas(p5.windowWidth - 10, p5.windowHeight - 10);
+    }
+    p5.setup = function () {
+        rate = 0.05;
+        canvas = p5.createCanvas(0, 0);
+        p5.strokeWeight(0.7);
+        p5.colorMode(p5.HSB, 1);
+        p5.angleMode(p5.DEGREES);
+        maxCircleWidth = 25;
+        resize();
     };
-    p.windowResized = function () {
-        p.resizeCanvas(p.windowWidth, p.windowHeight);
+    p5.windowResized = function () {
+        resize();
     };
-    p.draw = function () {
-        p.background(100);
-        p.ellipse(100, 100, 200, 200);
+    p5.draw = function () {
+        p5.background(p5.color(0, 0, 0.59));
+        p5.line(center.x, center.y, center.x + radius + maxCircleWidth, center.y);
+        var millis = window.performance.now();
+        var time = millis * rate;
+        for (var i = 0; i < NUM_POINTS; i++) {
+            var distance = (i + 1) / NUM_POINTS;
+            var angle = time * distance;
+            var len = radius * (1 + 1 / NUM_POINTS - distance);
+            var x = (center.x + p5.cos(angle) * len);
+            var y = (center.y + p5.sin(angle) * len);
+            var cWidth = maxCircleWidth - distance * 16;
+            var hue = distance;
+            var saturation = 1;
+            var brightness = 1;
+            p5.fill(p5.color(hue, saturation, brightness));
+            p5.ellipse(x, y, cWidth);
+        }
     };
 };
-var sketchP = new p5(sketch);
+new p5(sketch);
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,21 +1,67 @@
-var sketch = (p: p5) => {
+const sketch = (p5: p5) => {
+	const NUM_POINTS = 50;
+	let radius: number;
+	let center: { x: number, y: number };
+	let maxCircleWidth: number;
+	let canvas : HTMLCanvasElement;
+	let rate : number;
 
-	p.preload = () => {
+	p5.preload = () => { 
 
 	}
 
-	p.setup = () => {
-		p.createCanvas(p.windowWidth, p.windowHeight);
+	function resize() {
+		console.log(p5.windowWidth, p5.windowHeight);
+		radius = 0.8*(p5.windowWidth / 2);
+		center = {
+			x: p5.windowWidth / 2,
+			y: p5.windowHeight / 2
+		};
+		
+		// slightly less than full page to prevent overflow
+		p5.resizeCanvas(p5.windowWidth - 10, p5.windowHeight - 10);
 	}
 
-	p.windowResized = () => {
-		p.resizeCanvas(p.windowWidth, p.windowHeight);
+	p5.setup = () => {
+		rate = 0.05;
+		canvas = p5.createCanvas(0, 0);
+		p5.strokeWeight(0.7);
+		p5.colorMode(p5.HSB, 1);
+		p5.angleMode(p5.DEGREES);
+		maxCircleWidth = 25;
+		resize(); // initialize canvas size and variables		
 	}
 
-	p.draw = () => {
-		p.background(100);
-		p.ellipse(100,100,200,200);
+	p5.windowResized = () => {
+		resize();
+	}
+
+	p5.draw = () => {
+		p5.background(p5.color(0,0,0.59));		
+		p5.line(center.x, center.y, center.x + radius + maxCircleWidth, center.y);
+
+		// @ts-ignore millis between each frame
+		//let deltaTime = window.performance.now() - canvas._pInst._lastFrameTime;
+		let millis = window.performance.now();
+		let time = millis * rate;
+
+		for (let i = 0; i < NUM_POINTS; i++) {
+			let distance = (i + 1) / NUM_POINTS;
+			let angle = time * distance;
+			let len = radius * (1 + 1 / NUM_POINTS - distance);
+
+			let x = (center.x + p5.cos(angle) * len);
+			let y = (center.y + p5.sin(angle) * len);
+			
+			let cWidth = maxCircleWidth - distance * 16;
+			let hue = distance;
+			let saturation = 1;
+			let brightness = 1;
+
+			p5.fill(p5.color(hue, saturation, brightness));
+			p5.ellipse(x, y, cWidth);
+		}
 	}
 }
 
-var sketchP = new p5(sketch);
+new p5(sketch);
